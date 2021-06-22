c0d85a8250a8122260c8d61f81a91db6a80bd1c7
meg-engineer <cyobrin@gmail.com>
2021-06-22 13:17:51 +0900

    Hero修正

--- components/Hero/index.jsx
+++ components/Hero/index.jsx
@@ -1,9 +1,11 @@
+import Image from "next/image";
+
 export default function Hero() {
   return (
     <div className="hero">
       <div className="text-center hero-content">
         <div className="max-w-md py-5">
-          <h1 className="mb-5 text-4xl font-bold">Meg Blog</h1>
+          <Image src="/images/yatta.png" width={150} height={150} />
           <p className="mb-5 text-sm text-gray-500">
             エンジニアライフ、技術的なアウトプットを中心に発信します。
           </p>
