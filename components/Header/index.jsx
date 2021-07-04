47c0cd5b98ba3350263b59ccaa53e75238ee3a6a
cyobrin@gmail.com <cyobrin@gmail.com>
2021-07-05 00:26:59 +0900

    HeaderアイコンStyle修正

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -12,23 +12,27 @@ export default function Header() {
             <a className="text-4xl font-bold hover:text-gray-500">Meg Blog</a>
           </Link>
         </div>
-        <div className="pt-4 mx-2">
-          <a
-            href="https://github.com/meg-engineer"
-            className="hover:text-gray-500 mr-3"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon icon={faGithub} />
-          </a>
-          <a
-            href="https://twitter.com/MegEngineer"
-            className="hover:text-gray-500"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon icon={faTwitter} />
-          </a>
+        <div className="pt-4 flex justify-between">
+          <div>
+            <a
+              href="https://github.com/meg-engineer"
+              className="hover:text-gray-500 mr-5"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faGithub} size="lg" />
+            </a>
+          </div>
+          <div>
+            <a
+              href="https://twitter.com/MegEngineer"
+              className="hover:text-gray-500"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faTwitter} size="lg" />
+            </a>
+          </div>
         </div>
       </div>
     </div>
